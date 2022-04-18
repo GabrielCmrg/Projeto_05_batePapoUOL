873726a0e18acc60c9c8da9e456dd2457d6b0b9d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-04-18 18:05:21 -0300

    Bonus feito

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function login(person) {
-    const obj = {name: person.name};
+function login() {
+    user.name = document.querySelector(".load > input").value;
+    const obj = {name: user.name};
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", obj);
     promise.then(startLoadingMessages).catch(showErrorMessages);
 }
@@ -114,17 +115,45 @@ function showHideSideMenu() {
     document.querySelector(".overlay").classList.toggle("hidden");
 }
 
+function changeToWho() {
+    const toWho = document.querySelector(".to-who");
+    if (selectedVisibility.innerText.trim() == "Público") {
+        toWho.innerText = `Enviando para ${selectedContact}`;
+    } else {
+        toWho.innerText = `Enviando para ${selectedContact} (reservadamente)`;
+    }
+}
+
 function select(element) {
+    element.querySelector(".checkmark").classList.remove("hidden");
+    const toWho = document.querySelector(".to-who");
+
     if (document.querySelector(".contacts").contains(element)) {
-        if (selectedContact.innerText === element.innerText) {
+        if (selectedContact === undefined) {
+            selectedContact = element.innerText.trim();
+            changeToWho();
+
+            return;
+        } else {
+            if (selectedContact === element.innerText.trim()) {
+                selectedContact = element.innerText.trim();
+                changeToWho();
+
+                return;
+            }
+
+            const contactsList = document.querySelectorAll(".contacts li");
+            for (let i = 0; i < contactsList.length; i++) {
+                if (contactsList[i].innerText.trim() === selectedContact) {
+                    contactsList[i].querySelector(".checkmark").classList.add("hidden");
+                    selectedContact = element.innerText.trim();
+                    changeToWho();
+                    break;
+                }
+            }
+
             return;
         }
-
-        selectedContact.querySelector(".checkmark").classList.add("hidden");
-        element.querySelector(".checkmark").classList.remove("hidden");
-        selectedContact = element;
-
-        return;
     }
 
     if (selectedVisibility.innerText === element.innerText) {
@@ -132,13 +161,65 @@ function select(element) {
     }
 
     selectedVisibility.querySelector(".checkmark").classList.add("hidden");
-    element.querySelector(".checkmark").classList.remove("hidden");
     selectedVisibility = element;
+    changeToWho();
+}
+
+function loadParticipants() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    promise.then(refreshParticipantsList);
+}
+
+function refreshParticipantsList(response) {
+    const participantsList = document.querySelector(".contacts");
+    participantsList.innerHTML = `
+    <li onclick="select(this)">
+        <div class="name">
+            <ion-icon name="people"></ion-icon>Todos
+        </div>
+        <ion-icon name="checkmark-outline" class="checkmark hidden"></ion-icon></ion-icon>
+    </li>`;
+    let lastSelected = selectedContact;
+    selectedContact = undefined;
+
+    let participant;
+
+    for (let i = 0; i < response.data.length; i++) {
+        renderParticipant(response.data[i]);
+        participant = participantsList.querySelector("li:last-child");
+        if (lastSelected !== undefined && participant.innerText.trim() === lastSelected) {
+            select(participant);
+        }
+    }
+
+    if (selectedContact === undefined) {
+        const all = document.querySelector(".contacts li");
+        select(all);
+        selectedContact = all.innerText.trim();
+    }
+}
+
+function renderParticipant(participant) {
+    const participantsList = document.querySelector(".contacts");
+
+    participantsList.innerHTML += `
+    <li onclick="select(this)">
+        <div class="name">
+            <ion-icon name="person-circle"></ion-icon>${participant.name}
+        </div>
+        <ion-icon name="checkmark-outline" class="checkmark hidden"></ion-icon></ion-icon>
+    </li>`;
+}
+
+function hideLogin() {
+    document.querySelector(".load").classList.add("hidden");
+    login();
 }
 
 const messageBox = document.querySelector(".msg-box");
-let selectedContact = document.querySelector(".contacts li");
+let selectedContact = undefined;
 let selectedVisibility = document.querySelector(".visibilities li");
-//let user = {name: prompt("Qual seu nome?"), messagesID: 1, loginID: 2};
 
-//login(user);
+loadParticipants();
+let participantsID = setInterval(loadParticipants, 10000);
+let user = {name: "dummy", messagesID: 1, loginID: 2};
